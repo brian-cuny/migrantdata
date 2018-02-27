6c2a3733857f3de8536189af9bb20f27e0a85d97
brian.weinfeld+cuny@gmail.com <brian.weinfeld+cuny@gmail.com>
2018-02-27 16:42:01 -0500

    rough draft

--- migrationrates.R
+++ migrationrates.R
@@ -2,43 +2,51 @@ library(tidyverse)
 library(magrittr)
 library(zoo)
 
-raw.data <- read.csv('C:\\Users\\Brian\\Desktop\\GradClasses\\Spring18\\607\\projects\\UN_MigrantStockByAge_2015.csv', stringsAsFactors=FALSE, skip=14, header=FALSE, na.strings='..') 
 
-raw.data %<>%
-  subset(select=-c(1, 3:4, 6:22, 39, 56))
-
-column.names <- c('country', 'code', paste('m', raw.data[2, 3:18]), paste('f', raw.data[2, 19:34])) %>% unlist() %>% setNames(NULL)
-
-raw.data %<>% .[-(1:8), ] %>% setNames(column.names) %>%
+# read in data, remove uneeded parts and set column names -----------------
+raw.data <- read.csv('C:\\Users\\Brian\\Desktop\\GradClasses\\Spring18\\migrantdata\\UN_MigrantStockByAge_2015.csv', 
+                     stringsAsFactors=FALSE, header=FALSE, na.strings='..') %>%
+  .[-c(1:15, 17:22), -c(1, 3:4, 6:22, 39, 56)] %>%
+  setNames(c('country', 'code', paste('m', .[1, 3:18]), paste('f', .[1, 19:34])) %>% unlist()) %>%
+  .[-1, ] %>%
   na.omit()
+# read in data, remove uneeded parts and set column names -----------------
 
-area <- raw.data %>% subset(code == '', select=c('country')) 
+# create list of major and sub group locations ----------------------------
+all.groupings <- raw.data %>% 
+  subset(code == '', select=c('country')) 
 
-group <- area[c(1, 7, 13, 18, 23), ]
-subgroup <- area[-c(1, 7, 13, 18, 23), ]
-area %<>% mutate(big = replace(.$country, !.$country %in% group, NA))
-area %<>% mutate(small = replace(.$country, !.$country %in% subgroup, NA))
+major.group.locs <- c(1, 7, 13, 18, 23)
+major.group <- all.groupings[major.group.locs, ]
+sub.group <- all.groupings[-major.group.locs, ]
 
-area$big %<>% na.locf()
+all.groupings %<>% mutate(major.group = replace(.$country, !.$country %in% major.group, NA) %>% na.locf(), 
+                          sub.group = replace(.$country, !.$country %in% sub.group, NA)
+                         ) %>%
+  subset(!is.na(sub.group), -1)
+# create list of major and sub group locations ----------------------------
 
-area %<>% subset(!is.na(small), select=-1)
+# create mapping of major and sub groups ----------------------------------
+raw.data %<>% subset(!.$country %in% major.group) %>% .[-1, ]
 
-raw.data %<>% subset(!.$country %in% group) %>% .[-1, ]
-
-final.country <- data.frame(rep(NA, nrow(raw.data)), rep(NA, nrow(raw.data)))
+country.mapping <- data.frame(rep(NA, nrow(raw.data)), rep(NA, nrow(raw.data)))
 for(i in 1:nrow(raw.data)){
   if(raw.data$code[i] == ''){
-    area %<>% .[-1, ]
+    all.groupings %<>% .[-1, ]
   }else{
-    final.country[i, ] <- area[1, ]
+    country.mapping[i, ] <- all.groupings[1, ]
   }
 }
+# create mapping of major and sub groups ----------------------------------
 
+
+# combine data frames, set column names, and reorder content --------------
 answer <- cbind(final.country, raw.data) %>%
-  subset(code != '', select=-4) %>% 
-  setNames(c('group', 'subgroup', 'country', names(raw.data)[3:length(raw.data)])) %>%
-  .[, c(3, 1, 2, 4:35)]
+  setNames(c('group', 'subgroup', 'country', names(raw.data)[2:length(raw.data)])) %>%
+  subset(code != '') %>% 
+  .[, c(3, 1, 2, 5:36)]
 
 answer[, 4:35] %<>% map(~str_replace_all(., '\\s', '') %>% as.numeric())
+# combine data frames, set column names, and reorder content --------------
 
 write.csv(answer, 'answer.csv')
